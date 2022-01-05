7a29ecfbab9c95b20bfdacf64e3edb5324c6f07c
Guilherme Silva <guilhermebaos@gmail.com>
2022-01-05 16:33:58 +0000

    Animação Estatísticas BEST

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function dropdown(delay=0) {
 // Sticky navbar
 const navbar = document.getElementById('navbar')
 const offset = navbar.offsetTop
-window.onscroll = () => {
+window.addEventListener('scroll', () => {
     if (window.scrollY >= offset) {
         navbar.style.position = 'fixed'
         navbar.style.top = '-3px'
@@ -29,6 +29,69 @@ window.onscroll = () => {
         navbar.style.position = 'absolute'
         navbar.style.top = `${offset}px`
     }
-    console.log(window.scrollY)
-    console.log(offset)
+})
+
+
+// Animate numbers going up
+const animationDuration = 4000          // Duration of the animation in ms
+const animationSteps = 200               // Number of steps in the animation
+const animationTargets = [
+    [document.getElementById('num-countries'), 32],
+    [document.getElementById('num-groups'), 94],
+    [document.getElementById('num-students'), 3300]
+]
+
+// This is the easing function: https://easings.net/#easeOutQuint
+function easingFunction(t) {
+    return 1 - (1-t)**5
 }
+
+// Check if an element is on the viewport
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect()
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    )
+}
+
+// Do the animation
+const numberBoxes = document.getElementById('number-boxes')
+
+let animated = false
+function animateNumbers() {
+
+    // If the animation has been done, return
+    if (animated) return
+
+    // If the object is not visible, return
+    let visible = isInViewport(numberBoxes)
+    if (!visible) return
+
+    animated = true
+    
+    let delay = animationDuration / animationSteps
+
+    animationTargets.forEach((thisCounter, index) => {
+        const updateNumber = (currentStep = 0) => {
+            let htmlObject = thisCounter[0]
+            let numTarget = thisCounter[1]
+
+            if (currentStep < animationSteps) {
+                let time = currentStep / animationSteps
+    
+                htmlObject.innerText = Math.ceil(easingFunction(time) * numTarget)
+    
+                window.setTimeout(updateNumber, delay, currentStep + 1)
+            } else {
+                htmlObject.innerText = numTarget
+            }
+        }
+
+        updateNumber()
+    })
+}
+
+window.addEventListener('scroll', animateNumbers)
